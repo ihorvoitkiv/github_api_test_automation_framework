e220652d7cb7cccd55f527b68d11950329bd80b9
ihor_voitkiv <i.voitkiv27@gmail.com>
2023-04-04 13:37:21 +0300

    added ApiDataProvider class

---
+++ src/data_generation.py
@@ -0,0 +1,11 @@
+class ApiDataProvider:
+    """
+    A class for generating data via an API for use as a precondition flow in a test case and for further assertions.
+    """
+
+
+
+
+
+if __name__ == "__main__":
+    pass
