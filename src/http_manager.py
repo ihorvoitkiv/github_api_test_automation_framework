861078747bc12c26189c893928b41bc4dc433665
ihor_voitkiv <i.voitkiv27@gmail.com>
2023-04-21 17:19:47 +0300

    added pprint

--- src/http_manager.py
+++ src/http_manager.py
@@ -3,6 +3,8 @@ import logging
 
 import requests
 
+import pprint
+
 logger = logging.getLogger(__name__)
 
 
@@ -11,25 +13,23 @@ def log_request(func):
     def wrapper(*args, **kwargs):
         request_method = func.__name__.upper()
         request_url = args[1]
-
-        logging.info(f"------------------------- Request -------------------------")
-        logging.info(f"Method {request_method}, URL: {request_url}")
-        logging.debug(f"Headers:\n{kwargs.get('headers')}")
-        logging.debug(f"Params:\n{kwargs.get('params')}")
-        logging.debug(f"Data:\n{kwargs.get('data')}")
-        logging.debug(f"JSON:\n{kwargs.get('json')}")
-
+        logger.info(f"{'-' * 25} Request {'-' * 25}")
+        logger.info(f"Request: {request_method} {request_url}")
+        logger.debug(f"Headers:\n{pprint.pformat(kwargs.get('headers', {}))}")
+        logger.debug(f"Params:\n{pprint.pformat(kwargs.get('params', {}))}")
+        logger.debug(f"Data:\n{pprint.pformat(kwargs.get('data'))}")
+        logger.debug(f"JSON:\n{pprint.pformat(kwargs.get('json'))}")
         try:
             response = func(*args, **kwargs)
-            logging.info(f"------------------------- Response -------------------------")
-            logging.info(f"Status code: {response.status_code}, {response.reason}")
-            logging.debug(f"Body:\n{response.text}")
-            logging.debug(f"Headers:\n{response.headers}")
+            logger.info(f"{'-' * 25} Response {'-' * 25}")
+            logger.info(f"Response: {response.status_code} {response.reason}")
+            logger.debug(f"Headers:\n{pprint.pformat(dict(response.headers))}")
+            logger.debug(f"Body:\n{pprint.pformat(response.text)}")
             return response
         except Exception as e:
-            logging.error(f"Error requesting {request_method}: {e}")
+            logger.error(f"Error requesting {request_method}: {e}")
         finally:
-            logging.info(f"{'=' * 100}\n")
+            logger.info("=" * 100)
 
     return wrapper
 
