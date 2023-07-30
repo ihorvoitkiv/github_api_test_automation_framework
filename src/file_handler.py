cebc5219fe6f0bfa128333b736780d17306655da
Ihor Voitkiv <60190914+ihorvoitkiv@users.noreply.github.com>
2023-07-30 12:11:03 +0300

    Update file_handler.py

--- src/file_handler.py
+++ src/file_handler.py
@@ -1,39 +1,40 @@
-import json
 import os
+import json
 from abc import ABC, abstractmethod
 
 
-class File(ABC):
-    def __init__(self, filename, data_dir=None):
-        self.filename = filename
-        self.data_dir = data_dir
+class BaseFile(ABC):
+    """
+    BaseFile is an abstract class representing a file.
+    Specific file formats handling is implemented in child classes.
 
-    def read(self):
-        with open(self._get_file_path(), 'r') as file:
-            return self._read(file)
+    Attributes:
+        file_path (str): The full path to the file, default data_dir is '.src/data'
+    """
 
-    def write(self, data):
-        with open(self._get_file_path(), 'w') as file:
-            self._write(file, data)
-        return self
-
-    def _get_file_path(self):
-        current_dir = os.path.abspath(os.path.dirname(__file__))
-        data_dir = self.data_dir or os.path.join(current_dir, 'data')
-        return os.path.join(data_dir, self.filename)
+    def __init__(self, filename: str, data_dir: str = '../data') -> None:
+        self.file_path = os.path.join(os.getcwd(), data_dir, filename)
 
     @abstractmethod
-    def _read(self, file):
+    def read(self) -> dict:
+        """
+        Abstract method for reading the contents of the file.
+        """
         pass
 
     @abstractmethod
-    def _write(self, file, data):
+    def write(self, data: dict) -> None:
+        """
+        Abstract method for writing data to the file.
+        """
         pass
 
 
-class JSONFile(File):
-    def _read(self, file):
-        return json.load(file)
+class JSONFile(BaseFile):
+    def read(self) -> dict:
+        with open(self.file_path, 'r') as file:
+            return json.load(file)
 
-    def _write(self, file, data):
-        json.dump(data, file)
+    def write(self, data: dict) -> None:
+        with open(self.file_path, 'w') as file:
+            json.dump(data, file, indent=2)
